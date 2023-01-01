de2721b09c593599ea44aa15b0ae80f3d09cae9e
ttachi <ttachi@student.42tokyo.jp>
2023-01-01 18:48:55 +0900

    second commit

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,20 @@
 /*   By: ttachi <ttachi@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 23:03:13 by ttachi            #+#    #+#             */
-/*   Updated: 2022/12/31 23:03:14 by ttachi           ###   ########.fr       */
+/*   Updated: 2023/01/01 17:14:18 by ttachi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1
+#endif
+
+char *get_next_line(int fd);
+
+#endif
